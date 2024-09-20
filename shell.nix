69913b23e2e1e5689dbc00090cb419b8badcd1b5
Nikita Skakun <nikita@skakun.io>
2024-09-20 15:00:35 -0700

    Clean up unused environment variables

--- shell.nix
+++ shell.nix
@@ -2,22 +2,16 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    cmake # CMake for configuring the project
     clang # Clang compiler
+    cmake # CMake for configuring the project
     git # Git for version control
     glew # GLEW for handling OpenGL extensions
     glfw # GLFW3 for creating windows and OpenGL contexts
     gnumake # Make for building the project
     libxkbcommon # Keymap handling library (shared between X11 and Wayland)
+    mesa # Add Mesa for OpenGL support
     pkg-config # pkg-config to manage library paths
     wayland # Wayland development libraries
     wayland-protocols # Wayland protocols for Wayland support
-    mesa # Add Mesa for OpenGL support
   ];
-
-  # Set Clang as the default compiler
-  shellHook = ''
-    export CC=${pkgs.clang}/bin/clang
-    export CXX=${pkgs.clang}/bin/clang++
-  '';
 }
