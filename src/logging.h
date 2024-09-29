e5b6e0ae1e001c9475111a914cc0e9a3701f337e
Nikita Skakun <nikita@skakun.io>
2024-09-29 07:27:37 +0000

    Add back Clang-18 to CodeQL, make logging function constexpr

--- src/logging.h
+++ src/logging.h
@@ -4,7 +4,7 @@
 #include <termcolor/termcolor.hpp>
 
 // Enum for log levels
-enum class LogLevel
+enum class LogLevel : u_int8_t
 {
     INFO,
     WARNING,
@@ -12,5 +12,31 @@ enum class LogLevel
     DEBUG
 };
 
-// Function to log messages with colors
-void LogMessage(LogLevel level, const std::string &message);
+/**
+ * Logs a message to the console with a specific log level, formatting the output color based on the level.
+ *
+ * @param level The severity level of the log message (INFO, WARNING, ERROR, DEBUG).
+ * @param message The message to be logged.
+ */
+constexpr void LogMessage(LogLevel level, const std::string &message)
+{
+    if (message.empty())
+        return;
+
+    switch (level)
+    {
+    case LogLevel::INFO:
+        std::cout << termcolor::green << "[INFO] " << termcolor::reset;
+        break;
+    case LogLevel::WARNING:
+        std::cout << termcolor::yellow << "[WARNING] " << termcolor::reset;
+        break;
+    case LogLevel::ERROR:
+        std::cout << termcolor::red << "[ERROR] " << termcolor::reset;
+        break;
+    case LogLevel::DEBUG:
+        std::cout << termcolor::cyan << "[DEBUG] " << termcolor::reset;
+        break;
+    }
+    std::cout << message << std::endl;
+}
