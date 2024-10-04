f5be4d7c45c569ac5a5c10c16f180c7d1c265aa0
Nikita Skakun <nikita@skakun.io>
2024-10-04 13:09:35 +0000

    Add max file size to ReadFromFile (CWE-120, CWE-20)

--- src/fs_utils.hpp
+++ src/fs_utils.hpp
@@ -6,17 +6,22 @@
 #include <stdexcept>
 #include <vector>
 
+constexpr std::uintmax_t MAX_FILE_SIZE = 100 * 1024 * 1024; // 100 MB limit
+
 template <class CharContainer>
 constexpr CharContainer ReadFromFile(const std::filesystem::path &filepath)
 {
     if (!std::filesystem::exists(filepath))
         LogMessage(LogLevel::ERROR, std::format("File does not exist: {}", filepath.string()));
 
+    auto fileSize = std::filesystem::file_size(filepath);
+    if (fileSize > MAX_FILE_SIZE)
+        LogMessage(LogLevel::ERROR, std::format("File size exceeds maximum allowed size: {}", filepath.string()));
+
     std::ifstream file(filepath, std::ios::binary);
     if (!file)
         LogMessage(LogLevel::ERROR, std::format("Unable to open file: {}", filepath.string()));
 
-    auto fileSize = std::filesystem::file_size(filepath);
     CharContainer cc(fileSize);
 
     file.read(reinterpret_cast<char *>(cc.data()), fileSize);
