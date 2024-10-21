d9b1de92b135ff3e8a74167e37b8c129cd5a186b
Nikita Skakun <nikita@skakun.io>
2024-10-21 08:46:03 +0000

    feat(build): Prepare for proper audio implementation

--- shell.nix
+++ shell.nix
@@ -7,14 +7,20 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    alsa-lib # ALSA for audio handling
     clang # Clang compiler
     cmake # CMake for configuring the project
     git # Git for version control
     glew # GLEW for handling OpenGL extensions
     glfw # GLFW3 for creating windows and OpenGL contexts
     gnumake # Make for building the project
+    jack2 # JACK for audio handling
+    libogg # Ogg for audio handling
     libxkbcommon # Keymap handling library (shared between X11 and Wayland)
+    opusfile.dev # Opus for audio handling
     pkg-config # pkg-config to manage library paths
+    pulseaudio # PulseAudio for audio handling
+    rtaudio_6 # RtAudio for audio handling
     wayland # Wayland development libraries
     wayland-protocols # Wayland protocols for Wayland support
   ];
