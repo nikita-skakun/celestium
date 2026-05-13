0874319a54e641d504491a04c0fcb7865b863a79
Nikita Skakun <nikita@skakun.io>
2026-05-12 19:38:18 -0700

    ci: add missing X11 and OpenGL dependencies to shell.nix and enable overwrite flag for CodeQL database creation

--- shell.nix
+++ shell.nix
@@ -14,9 +14,17 @@ pkgs.mkShell {
     glfw # GLFW3 for creating windows and OpenGL contexts
     gnumake # Make for building the project
     jack2 # JACK for audio handling
+    libGL # OpenGL library
     libogg # Ogg for audio handling
     libopus # Opus for audio handling
+    libx11 # X11 library
+    libxcursor # XCursor library
+    libxext # X11 Extensions library
+    libxfixes # X11 Fixes library
+    libxi # Xi library
+    libxinerama # Xinerama library
     libxkbcommon # Keymap handling library (shared between X11 and Wayland)
+    libxrandr # XRandR library
     opusfile.dev # Opus for audio handling
     pkg-config # pkg-config to manage library paths
     pulseaudio # PulseAudio for audio handling
